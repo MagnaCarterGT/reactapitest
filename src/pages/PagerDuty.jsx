114941dae698630c7702286f2d9efe5a334c719c
Carter Templeton <ctempleton89@gmail.com>
2025-06-23 13:49:07 -0400

    get and display single user info

--- src/pages/PagerDuty.jsx
+++ src/pages/PagerDuty.jsx
@@ -1,8 +1,10 @@
-import {useEffect, useState} from 'react'
+import {useCallback, useEffect, useMemo, useState} from 'react'
 import '../App.css'
 import axios from 'axios';
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
-import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 
 function PagerDuty() {
     const [users, setUsers] = useState([]);
@@ -11,56 +13,154 @@ function PagerDuty() {
         page: 0,
         pageSize: 25,
     });
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState({});
+
+    const headers = {
+        'User-Agent': 'Mozilla/5.0',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Token token=y_NbAkKc66ryYTWUXYEu`
+    }
+
+    function getUser(user) {
+        let url = `https://api.pagerduty.com/users/${user.id}`;
+        return axios.get(url, {
+            headers
+        }).then(response => {
+            setUser(response.data.user);
+        })
+        .catch(error => {
+            console.error(error);
+            setIsLoading(false);
+        });
+    }
 
     function getUsers() {
         let url = `https://api.pagerduty.com/users`;
         return axios.get(url, {
-            headers: {
-            'User-Agent': 'Mozilla/5.0',
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Token token=y_NbAkKc66ryYTWUXYEu`
-            },
+            headers,
             params: {
                 limit: paginationModel.pageSize,
                 total: true,
                 offset: paginationModel.page * paginationModel.pageSize,
             }
         })
-            .then(response => {
-                console.log(response.data.total);
-                setUsers(response.data.users);
-                setTotalRows(response.data.total);
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        .then(response => {
+            setUsers(response.data.users);
+            setTotalRows(response.data.total);
+            setIsLoading(false);
+        })
+        .catch(error => {
+            console.error(error);
+            setIsLoading(false);
+        });
     }
 
+    const deleteUser = useCallback(
+        (id) => () => {
+            let url = `https://api.pagerduty.com/users/${id}`;
+            return axios.delete(url, {
+                headers
+            }).then(response => {
+                getUsers();
+            })
+                .catch(error => {
+                    console.error(error);
+                    setIsLoading(false);
+                });
+        },
+        [],
+    );
+
     useEffect(() => {
+        setIsLoading(true);
         getUsers();
     }, [paginationModel])
 
-    const columns = [
+    const columns = useMemo(
+        () => [
         { field: 'id', headerName: 'ID', width: 100 },
         { field: 'name', headerName: 'Name', width: 130 },
         { field: 'email', headerName: 'Email', width: 130 },
-    ];
+        {
+            field: 'actions',
+            type: 'actions',
+            width: 80,
+            getActions: (params) => [
+                <GridActionsCellItem
+                    icon={<DeleteIcon />}
+                    label="Delete"
+                    onClick={deleteUser(params.id)}
+                />,
+                // <GridActionsCellItem
+                //     icon={<SecurityIcon />}
+                //     label="Toggle Admin"
+                //     onClick={toggleAdmin(params.id)}
+                //     showInMenu
+                // />,
+                // <GridActionsCellItem
+                //     icon={<FileCopyIcon />}
+                //     label="Duplicate User"
+                //     onClick={duplicateUser(params.id)}
+                //     showInMenu
+                // />,
+            ]
+        },
+        // {
+        //     field: "",
+        //     headerName: "Delete",
+        //     sortable: false,
+        //     renderCell: (params) => {
+        //         const onClick = (e) => {
+        //             e.stopPropagation(); // don't select this row after clicking
+        //
+        //             const api = params.api;
+        //             const thisRow = {};
+        //
+        //             api
+        //                 .getAllColumns()
+        //                 .filter((c) => c.field !== "__check__" && !!c)
+        //                 .forEach(
+        //                     (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+        //                 );
+        //
+        //             return alert(JSON.stringify(thisRow, null, 4));
+        //         };
+        //
+        //         return <Button onClick={onClick}>Click</Button>;
+        //     }
+        // },
+    ], [deleteUser]
+    );
 
     return (
-        <Paper>
-            <DataGrid
-                rows={users}
-                rowCount={totalRows}
-                columns={columns}
-                pagination
-                paginationModel={paginationModel}
-                paginationMode="server"
-                pageSizeOptions={[5, 10, 25]}
-                sx={{ border: 0 }}
-                onPaginationModelChange={setPaginationModel}
-            />
-        </Paper>
+        <>
+            <Paper>
+                <DataGrid
+                    rows={users}
+                    rowCount={totalRows}
+                    columns={columns}
+                    pagination
+                    paginationModel={paginationModel}
+                    paginationMode="server"
+                    pageSizeOptions={[5, 10, 25]}
+                    sx={{ border: 0 }}
+                    onRowClick={getUser}
+                    onPaginationModelChange={setPaginationModel}
+                    loading={isLoading}
+                    slotProps={{
+                        loadingOverlay: {
+                            variant: 'skeleton',
+                            noRowsVariant: 'skeleton',
+                        },
+                    }}
+                />
+            </Paper>
+            <Paper elevation={0}>
+                Current user {JSON.stringify(user)}
+            </Paper>
+        </>
     )
 }
 
